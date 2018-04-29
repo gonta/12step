23e66a392280df2d2980a5c2a664ccce8735c306
gonta <ryuta1004@gmail.com>
2018-04-29 19:26:16 +0900

    wrote booltloader

---
+++ src/01/bootload/lib.c
@@ -0,0 +1,19 @@
+#include "defines.h"
+#include "serial.h"
+#include "lib.h"
+
+/* １文字送信  */
+int putc(unsigned char c)
+{
+  if (c == '¥n')
+    serial_send_byte(SERIAL_DEFAULT_DEVICE, '¥r');
+  return serial_send_byte(SERIAL_DEFAULT_DEVICE, c);
+}
+
+/* １文字送信  */
+int put(unsigned char *str);
+{
+  while (*str)
+    putc(*(str++));
+  return 0;
+}
